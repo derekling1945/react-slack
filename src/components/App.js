e12bb60706b46356861f90dfe2107d07c416a71b
derekling <derekling@Lings-Air.fritz.box>
2020-04-12 01:07:47 +0200

    Build Chat Console and Channel List

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
-export default App;
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: '#eee' }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+
+    <Grid.Column style={{marginLeft: 320}}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+    
+  </Grid>
+)
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,27 +3,60 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Spinner from './Spinner';
 import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser, clearUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      } else {
+        this.props.history.push('/login');
+        this.props.clearUser();
+      }
+    });
+  }
+
+  render() {
+    return this.props.isLoading? <Spinner /> : (
+      
+        <Switch>
+          <Route exact path="/" component={App}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+        </Switch>
+    );
+  }
+}
+
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
 
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App}></Route>
-      <Route path="/login" component={Login}></Route>
-      <Route path="/register" component={Register}></Route>
-
-
-    </Switch>
-  </Router>
-)
+const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser, clearUser })(Root));
 
 ReactDOM.render(
-    <Root />,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
